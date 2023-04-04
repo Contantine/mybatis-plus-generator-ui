de6c3ad1dd40b2111cc8f8a4a713ddee45d993cc
wangyongqi <m13587322831@163.com>
2023-04-04 16:03:37 +0800

    自定义模板

--- src/test/java/TestApplication.java
+++ src/test/java/TestApplication.java
@@ -11,16 +11,24 @@ public class TestApplication {
                 .password("root")
                 .port(8068)
                 .driverClassName("com.mysql.cj.jdbc.Driver")
-                .basePackage("com.github.davidfantasy.mybatisplus.generatorui.example")
+                //数据库schema，MSSQL,PGSQL,ORACLE,DB2类型的数据库需要指定
+//                .schemaName("MYSQL")
+                //如果需要修改entity及其属性的命名规则，以及自定义各类生成文件的命名规则，可自定义一个NameConverter实例，覆盖相应的名称转换方法，详细可查看该接口的说明：
                 .nameConverter(new NameConverter() {
+                    /**
+                     * 自定义Service类文件的名称规则
+                     */
                     @Override
                     public String serviceNameConvert(String tableName) {
                         return this.entityNameConvert(tableName) + "Service";
                     }
 
+                    /**
+                     * 自定义Controller类文件的名称规则
+                     */
                     @Override
                     public String controllerNameConvert(String tableName) {
-                        return this.entityNameConvert(tableName) + "Action";
+                        return this.entityNameConvert(tableName) + "Controller";
                     }
                 })
                 .build();
